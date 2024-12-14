07ff646ce43a36417521b412377f039a0e7a5468
Oketch <oketchngwani@gmail.com>
2024-12-15 04:37:38 +0900

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideDstnt){
+  let charges
+ 
+ if(rideDstnt <= 400) {
+  return "This is on me!";
+  
+ }
+ else if(rideDstnt>400 && rideDstnt<= 2000){
+  return "That will be twenty bucks.";
+  charges = $20;
+ }
+ 
+ else if (rideDstnt > 2000 && rideDstnt <= 2500){
+  return "I will gladly take your thirty bucks.";
+  charges = $30;
+ }
+
+ else {
+  return "No can do."
+ }
+}
+console.log(scuberGreetingForFeet(199));
+
+function ternaryCheckCity(city){
+    return city === "NYC" ? "Ok, sounds good!" : "No go.";
+  }
+  
+  // Example usage
+  console.log(ternaryCheckCity("NYC"));       // Output: Ok, sounds good!
+  console.log(ternaryCheckCity("Los Angeles"));// Output: No go.
+  
+
+
+
+
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+// Example usage
+console.log(switchOnCharmFromTip('generous'));        
+console.log(switchOnCharmFromTip('not as generous')); 
+console.log(switchOnCharmFromTip('cheap'));           
